0cb3a703b77ac8af3475ba2a91580830a9bd8c55
Don Pinkus <don.pinkus@gmail.com>
2022-07-08 19:11:37 -0700

    [Gallery] Denser thumbnails with better centering and auto-sizing

--- my-app/src/components/Gallery.js
+++ my-app/src/components/Gallery.js
@@ -3,54 +3,38 @@ import { useParams, Link } from 'react-router-dom';
 import { Text, Flex, Center } from '@chakra-ui/react';
 import { Feed } from './shared/Feed';
 
+const PaginationNav = ({ params }) => (
+  <Center mt={2} mb={2} fontSize="lg">
+    <Link
+      to={`/gallery/${params.user_id}/${params.amount}/${parseInt(
+        parseInt(params.page) - 1
+      )}`}
+    >
+      ◀️
+    </Link>
+
+    <Text mr={1} ml={1}>
+      {params.page}
+    </Text>
+
+    <Link
+      to={`/gallery/${params.user_id}/${params.amount}/${parseInt(
+        parseInt(params.page) + 1
+      )}`}
+    >
+      ▶️
+    </Link>
+  </Center>
+);
+
 export function Gallery() {
   const params = useParams();
 
   return (
     <>
-      <Center>
-        <Flex>
-          <Link
-            to={`/gallery/${params.user_id}/${params.amount}/${parseInt(
-              parseInt(params.page) - 1
-            )}`}
-          >
-            ◀️
-          </Link>
-          <Center>
-            <Text>{params.page}</Text>
-          </Center>
-          <Link
-            to={`/gallery/${params.user_id}/${params.amount}/${parseInt(
-              parseInt(params.page) + 1
-            )}`}
-          >
-            ▶️
-          </Link>
-        </Flex>
-      </Center>
+      <PaginationNav params={params} />
       <Feed user={params.user_id} amount={params.amount} page={1} />
-      <Center>
-        <Flex>
-          <Link
-            to={`/gallery/${params.user_id}/${params.amount}/${parseInt(
-              parseInt(params.page) - 1
-            )}`}
-          >
-            ◀️
-          </Link>
-          <Center>
-            <Text>{params.page}</Text>
-          </Center>
-          <Link
-            to={`/gallery/${params.user_id}/${params.amount}/${parseInt(
-              parseInt(params.page) + 1
-            )}`}
-          >
-            ▶️
-          </Link>
-        </Flex>
-      </Center>
+      <PaginationNav params={params} />
     </>
   );
 }
