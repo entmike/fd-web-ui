8d8c13e8d3bbb4bd948ee4adb4719c4f5fa0504a
Don Pinkus <don.pinkus@gmail.com>
2022-07-08 18:23:16 -0700

    [Piece Page][Bug fix][UX Change] Remove duplicate rendering of image. Update styles for rest of components to align better.

--- my-app/src/components/shared/DreamAuthor.js
+++ my-app/src/components/shared/DreamAuthor.js
@@ -1,27 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Image, Text, Box, Center, VStack } from '@chakra-ui/react';
+import { Image, Heading, Box, Center, VStack } from '@chakra-ui/react';
 
 export function DreamAuthor(props) {
   return (
-    <Box>
-      <Center>
-        <VStack marginTop="2" spacing="2" display="inline-block" width="100%">
-          <Link to={`/gallery/${props.userdets.user_str}/25/1`}>
-            <Image
-              borderRadius="full"
-              boxSize="60px"
-              src={
-                props.userdets.avatar
-                  ? props.userdets.avatar
-                  : '/avatar-placeholder.gif'
-              }
-              alt={`Avatar of ${props.userdets.user_name}`}
-            />
-            <Text>{props.userdets.user_name}</Text>
-          </Link>
-        </VStack>
-      </Center>
-    </Box>
+    <Link to={`/gallery/${props.userdets.user_str}/25/1`}>
+      <VStack mb={1}>
+        <Image
+          borderRadius="full"
+          boxSize="60px"
+          src={
+            props.userdets.avatar
+              ? props.userdets.avatar
+              : '/avatar-placeholder.gif'
+          }
+          alt={`Avatar of ${props.userdets.user_name}`}
+        />
+        <Heading as="h3" size="md">
+          {props.userdets.user_name}
+        </Heading>
+      </VStack>
+    </Link>
   );
 }
