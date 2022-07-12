262ceaa697732aee3576654da89ae6104e86bcb0
entmike <mike.howles@gmail.com>
2022-07-12 23:20:18 +0000

    UI Stubs

--- my-app/src/components/Pages/PiecePage.js
+++ my-app/src/components/Pages/PiecePage.js
@@ -147,20 +147,22 @@ function PiecePage({ token }) {
                     size="xs"
                     onClick={() => (window.location.href = ``)}
                     ml={1}
+                    isDisabled
                   >
                     <svg xmlns="http://www.w3.org/2000/svg" className="heroicons-md" viewBox="0 0 20 20" fill="currentColor">
                       <path fillRule="evenodd" d="M3.172 5.172a4 4 0 015.656 0L10 6.343l1.172-1.171a4 4 0 115.656 5.656L10 17.657l-6.828-6.829a4 4 0 010-5.656z" clipRule="evenodd" />
-                    </svg> 18
+                    </svg> (Coming Soon)
                   </Button>
                   <Button
                     colorScheme={'purple'}
                     size="xs"
                     onClick={() => (window.location.href = ``)}
                     ml={1}
+                    isDisabled
                   >
                     <svg xmlns="http://www.w3.org/2000/svg" className="heroicons-md" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
                       <path strokeLinecap="round" strokeLinejoin="round" d="M8.684 13.342C8.886 12.938 9 12.482 9 12c0-.482-.114-.938-.316-1.342m0 2.684a3 3 0 110-2.684m0 2.684l6.632 3.316m-6.632-6l6.632-3.316m0 0a3 3 0 105.367-2.684 3 3 0 00-5.367 2.684zm0 9.316a3 3 0 105.368 2.684 3 3 0 00-5.368-2.684z" />
-                    </svg> Share
+                    </svg> (Coming Soon)
                   </Button>
                   <Button
                     colorScheme={'green'}
