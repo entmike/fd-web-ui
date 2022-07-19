a43c39ea4de60255d26ede581c35266d2a4cbe4e
Mike Howles <entmike@gmail.com>
2022-07-19 10:39:57 -0500

    error trapping

--- my-app/src/utils/dateUtils.js
+++ my-app/src/utils/dateUtils.js
@@ -1,8 +1,12 @@
 import { format } from "date-fns"
 export function dt(ts) {
-  if (ts && ts["$date"]) {
-    return format(new Date(ts.$date), "MM/dd/yyyy k:mm:ss")
-  } else {
-    return format(new Date(ts), "MM/dd/yyyy k:mm:ss")
+  try{
+    if (ts && ts["$date"]) {
+      return format(new Date(ts.$date), "MM/dd/yyyy k:mm:ss")
+    } else {
+      return format(new Date(ts), "MM/dd/yyyy k:mm:ss")
+    }
+  }catch(e){
+    return "Invalid date format"
   }
 }
